7b37f7c1af2c103248dbd51c1257d5360ce2de9b
Teayeon-Lim <wingofyeon@naver.com>
2021-09-27 16:52:28 +0900

    링크 이동(Link)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - yarn add react-router-dom
 - [메인 페이지]와 [페이지 1]에 router 사용하기
+
+# Use react-router
+
+- 링크로 이동 시, 'a' 태그 사용하면 안됨(페이지 전체가 렌더링)
+  => Link 사용.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Route } from 'react-router';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to='/about'>소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path='/' component={Home} exact></Route>
       <Route path='/about' component={About}></Route>
     </div>
